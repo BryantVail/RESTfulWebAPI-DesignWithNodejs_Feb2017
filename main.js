66e57284587b7d6a00cb7a730e70c3587d2d1954
bxvail <BryantVail@gmail.com>
2018-04-07 21:14:52 -0400

    init - finish, notes only
    finish

---
+++ main.js
@@ -0,0 +1,28 @@
+//main.js
+/*
+    SECTION OBJECTIVES
+        API defined
+        HTTP communication
+        Resources in REST
+        RESTful API Properties
+        Dev Environment
+*/
+
+/*
+    VIDEO OBJECTIVES
+        Video 1.2
+        What is an API
+            Set of routines, protocols, and tools for building software & applications
+            An interface
+        What are APIs used for
+            performing tasks
+                twitter's api
+            retrieving data
+                friend's list
+            manipulating data
+                adding a contact
+        What are web APIs
+            Interface that runs on a network
+                the data to be accessed is on another machine
+
+*/
